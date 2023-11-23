68397241f562581d0960cf6d5c2ccfdef1f7e913
Ihor Hryshchenko <ihryshchenko@mirantis.com>
2023-11-23 23:48:12 +0200

    del tests for jest

--- dist/index.js
+++ dist/index.js
@@ -6186,16 +6186,18 @@ exports["default"] = _default;
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
 const core = __nccwpck_require__(2186)
-const axios = __nccwpck_require__(8757);
+const axios = __nccwpck_require__(8757)
 
 async function getRandomJoke() {
   try {
-    const response = await axios.get('https://official-joke-api.appspot.com/random_joke');
+    const response = await axios.get(
+      'https://official-joke-api.appspot.com/random_joke'
+    )
     core.debug(response)
-    return response.data;
+    return response.data
   } catch (error) {
-    console.error('Error fetching joke:', error.message);
-    throw error;
+    console.error('Error fetching joke:', error.message)
+    throw error
   }
 }
 
@@ -6205,30 +6207,15 @@ async function sendJokeToTelegram(joke, botToken, chatId) {
     const customJoke = `
       CUSTOM: ${joke.setup}
       ${joke.delivery}
-    `;
-    const encodedJoke = encodeURIComponent(customJoke);
-    const url = `https://api.telegram.org/${botToken}/sendMessage?chat_id=${chatId}&parse_mode=MarkdownV2&text=${encodedJoke}`;
-    
-    await axios.get(url);
-    console.log('Joke sent to Telegram successfully.');
+    `
+    const encodedJoke = encodeURIComponent(customJoke)
+    const url = `https://api.telegram.org/${botToken}/sendMessage?chat_id=${chatId}&parse_mode=MarkdownV2&text=${encodedJoke}`
+
+    await axios.get(url)
+    console.log('Joke sent to Telegram successfully.')
   } catch (error) {
-    console.error('Error sending joke to Telegram:', error.message);
-    throw error;
-  }
-}
-
-async function run() {
-  try {
-    const chatID = core.getInput('chat_id', { required: true })
-    const token = core.getInput('token', { required: true })
-    const joke = await getRandomJoke();
-
-    core.setOutput('joke', joke)
-
-    await sendJokeToTelegram(joke, token, chatID);
-  } catch (error) {
-    console.error('Workflow failed:', error.message);
-    process.exit(1);
+    console.error('Error sending joke to Telegram:', error.message)
+    throw error
   }
 }
 
@@ -6240,11 +6227,11 @@ async function run() {
   try {
     const chatID = core.getInput('chat_id', { required: true })
     const token = core.getInput('token', { required: true })
-    const joke = await getRandomJoke();
+    const joke = await getRandomJoke()
 
     core.setOutput('joke', joke)
 
-    await sendJokeToTelegram(joke, token, chatID);
+    await sendJokeToTelegram(joke, token, chatID)
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 const core = require('@actions/core')
-const axios = require('axios');
+const axios = require('axios')
 
 async function getRandomJoke() {
   try {
-    const response = await axios.get('https://official-joke-api.appspot.com/random_joke');
+    const response = await axios.get(
+      'https://official-joke-api.appspot.com/random_joke'
+    )
     core.debug(response)
-    return response.data;
+    return response.data
   } catch (error) {
-    console.error('Error fetching joke:', error.message);
-    throw error;
+    console.error('Error fetching joke:', error.message)
+    throw error
   }
 }
 
@@ -18,30 +20,15 @@ async function sendJokeToTelegram(joke, botToken, chatId) {
     const customJoke = `
       CUSTOM: ${joke.setup}
       ${joke.delivery}
-    `;
-    const encodedJoke = encodeURIComponent(customJoke);
-    const url = `https://api.telegram.org/${botToken}/sendMessage?chat_id=${chatId}&parse_mode=MarkdownV2&text=${encodedJoke}`;
-    
-    await axios.get(url);
-    console.log('Joke sent to Telegram successfully.');
+    `
+    const encodedJoke = encodeURIComponent(customJoke)
+    const url = `https://api.telegram.org/${botToken}/sendMessage?chat_id=${chatId}&parse_mode=MarkdownV2&text=${encodedJoke}`
+
+    await axios.get(url)
+    console.log('Joke sent to Telegram successfully.')
   } catch (error) {
-    console.error('Error sending joke to Telegram:', error.message);
-    throw error;
-  }
-}
-
-async function run() {
-  try {
-    const chatID = core.getInput('chat_id', { required: true })
-    const token = core.getInput('token', { required: true })
-    const joke = await getRandomJoke();
-
-    core.setOutput('joke', joke)
-
-    await sendJokeToTelegram(joke, token, chatID);
-  } catch (error) {
-    console.error('Workflow failed:', error.message);
-    process.exit(1);
+    console.error('Error sending joke to Telegram:', error.message)
+    throw error
   }
 }
 
@@ -53,11 +40,11 @@ async function run() {
   try {
     const chatID = core.getInput('chat_id', { required: true })
     const token = core.getInput('token', { required: true })
-    const joke = await getRandomJoke();
+    const joke = await getRandomJoke()
 
     core.setOutput('joke', joke)
 
-    await sendJokeToTelegram(joke, token, chatID);
+    await sendJokeToTelegram(joke, token, chatID)
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
