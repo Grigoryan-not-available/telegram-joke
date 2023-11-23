13744422aaeb1c70f850fd90ab6ebc6d6e592c9e
Ihor Hryshchenko <ihryshchenko@mirantis.com>
2023-11-24 00:38:07 +0200

    Last try for today improve escapeChars

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ async function getRandomJoke() {
 async function sendJokeToTelegram(joke, botToken, chatId) {
   try {
     const escapeChars = str => {
-      return str.replace(/([.,-*+?^=!:${}()|\/\\])/g, '\\\\$1')
+      return str.replace(/([.,-*+?^=!:${}()|\/\\])/g, '\\$1')
     }
 
     const customJoke = `${escapeChars(joke.setup)}\n||${escapeChars(
